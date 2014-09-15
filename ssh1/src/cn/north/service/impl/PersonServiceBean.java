6bdf7ea12508367c5df67dc99acbdb5f5e7ab7d8
northyoung <joker.ssy@gmail.com>
2014-09-15 22:26:46 +0800

    增加配置文件

---
+++ ssh1/src/cn/north/service/impl/PersonServiceBean.java
@@ -0,0 +1,40 @@
+package cn.north.service.impl;
+
+import java.util.List;
+
+import cn.north.bean.Person;
+import cn.north.service.PersonService;
+
+public class PersonServiceBean implements PersonService {
+
+	@Override
+	public void save(Person person) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Person person) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Person getPerson(Integer personid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Integer personid) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Person> getPersons() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ssh1/src/junit/test/PersonServiceTest.java
@@ -0,0 +1,14 @@
+package junit.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
